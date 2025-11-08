e3848b6ddb3e82f84225bf99fe5ba736b1254eff
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 19:35:45 +0000

    add view , edit and delete booking functions

--- bookings/views.py
+++ bookings/views.py
@@ -17,3 +17,25 @@ def add_booking(request):
     else:
         form = BookingForm()
     return render(request, 'add_booking.html', {'form': form})        
+
+def view_bookings(request):
+    bookings = Booking.objects.all().order_by('-date', '-time')
+    return render(request, 'view_bookings.html', {'bookings': bookings})
+
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('view_bookings')
+    else:
+        form = BookingForm(instance=booking)
+    return render(request, 'edit_booking.html', {'form': form})        
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('view_bookings')
+    return render(request, 'delete_booking.html', {'booking': booking})
