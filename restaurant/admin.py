87356391342fa5629e072ab727d9c02e93b0194c
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 14:43:58 +0000

    register the booking class

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Restaurant, Review
+from .models import Restaurant, Booking
 
 # Register your models here.
 
 admin.site.register(Restaurant)
-admin.site.register(Review)
+admin.site.register(Booking)
