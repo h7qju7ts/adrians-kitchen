492a4236db926a5be4ed95353d87f4a57d86361a
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 19:21:21 +0000

    add booking function

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Booking
+from .forms import BookingForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def add_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_bookings')
+    else:
+        form = BookingForm()
+    return render(request, 'add_booking.html', {'form': form})        
