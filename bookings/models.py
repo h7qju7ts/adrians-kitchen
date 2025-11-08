2b9856389155817acffa7c327c564721c2a301b0
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 15:27:49 +0000

    add booking class to models.py

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    people = models.PositiveIntegerField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
