f13b63f0633fe9771d8154534b554ce38e2ef762
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 10:49:59 +0000

    add restaurant class to models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Restaurant(models.Model):
+    name = models.Charfield(max_length=200)
+    adress = models.CharField(max_length=300, blank=True)
+    description = models.TextField(blanl=True)
+
+    def __str__(self):
+        return self.name
+    
+    
