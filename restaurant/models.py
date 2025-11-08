d854d974efdf41a8efcd626f30cb6c3d503f2906
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-08 11:01:43 +0000

    migrated

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,9 +6,9 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 class Restaurant(models.Model):
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
     adress = models.CharField(max_length=300, blank=True)
-    description = models.TextField(blanl=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
