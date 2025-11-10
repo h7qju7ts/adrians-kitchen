b94777070c00f0e1e2aaf029a248a581d4d4d190
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-10 11:39:01 +0000

    style rest of elements and update template

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
     path('add/', views.add_booking, name='add_booking'),
     path('bookings/', views.view_bookings, name='view_bookings'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
