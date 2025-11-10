93786cba743dbf986097fbee755ce480454e2a9e
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-10 10:42:26 +0000

    add static folder +css file to bookings + import os to settings.py

--- adrians_kitchen/settings.py
+++ adrians_kitchen/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookings',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'bookings/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
