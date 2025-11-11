75531fff14d464da068e0fc7dc6f9c9db3493e87
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-11 16:52:47 +0000

    add logout redirect to home page

--- adrians_kitchen/settings.py
+++ adrians_kitchen/settings.py
@@ -136,3 +136,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login / Logout settings
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
+
