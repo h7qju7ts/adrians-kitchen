ddfeae617b75d8905b199a046de2173f942ea084
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-11 13:52:59 +0000

    add registration page + update forms.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Booking
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -10,3 +12,11 @@ class BookingForm(forms.ModelForm):
             'time': forms.TimeInput(attrs={'type': 'time'}),
                                     
         }
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]        
