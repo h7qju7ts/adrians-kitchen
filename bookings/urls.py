5f273cf398a70036cb8d93ccb6e617494ac482bd
Adrian <h7qju7ts@students.codeinstitute.net>
2025-11-11 14:04:27 +0000

    add register path to bookings/urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_booking, name='add_booking'),
+    path("register/", views.register, name="register"),
     path('bookings/', views.view_bookings, name='view_bookings'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
     path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
